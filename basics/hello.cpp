32c63fc90fd5cf1cdbbd2aad051675b76ba91939
Anthony Walker <walkanth@oregonstate.edu>
2021-12-29 15:17:40 -0500

    Add hello.cpp

---
+++ basics/hello.cpp
@@ -0,0 +1,11 @@
+// Author: Anthony Walker
+// This is a hello world c++ example for comparison to python in the first lecture.
+// This file can be compiled and ran with bash shell commands:
+// g++ hello.cpp -o hello
+// ./hello
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+    std::cout<<"Hello, World!"<<std::endl;
+}
